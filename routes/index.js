b1c55bd20eded62abb477cad3002de3818fd2447
Nur Bar <LCD344@yahoo.com>
2018-06-01 21:53:26 +0300

    fixed small error

--- routes/index.js
+++ routes/index.js
@@ -25,8 +25,8 @@ router.post('/api/contact', csrfProtection, function (req, res) {
 		}
 	});
 	var mailOptions = {
-		from: '"Contact Form" <contact@elcoop.io>', // sender address
-		to: 'contact.@elcoop.io', // list of receivers
+		from: '"Contact Form" <' + process.env.MAIL_USER + '>', // sender address
+		to: process.env.MAIL_USER, // list of receivers
 		subject: 'Message from ' + req.body.name, // Subject line
 		text: req.body.name + '\n' + req.body.email + '\n' + req.body.message, // plain text body
 		html: '<b>Message from: </b>' + req.body.name + '<br>Email: ' + req.body.email + '<br><br>' + req.body.message, // html body,
